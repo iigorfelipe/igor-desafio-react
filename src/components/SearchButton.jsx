156a5f503397095934ab22ca75188ea8bcb80e57
Igor <iigorfelipe@gmail.com>
2022-08-05 11:26:04 -0300

    feat: save user information in state

--- src/components/SearchButton.jsx
+++ src/components/SearchButton.jsx
@@ -1,9 +1,29 @@
-import React from "react";
+import React, { useContext } from "react";
+import getUser from "../apis/github/request/getUser";
+import GithubContext from "../providers/GithubContext";
+import getRepository from "../apis/github/request/getRepository";
 
-const SearchButton = () => (
-  <button type="button" onClick={() => console.log("Clicou")}>
-    Buscar
-  </button>
-);
+const SearchButton = () => {
+  const { searchInputValue, setUserInformation } = useContext(GithubContext);
+
+  const saveUsersInformation = async (username) => {
+    const dataUser = await getUser(username);
+    const dataRepo = await getRepository(username);
+
+    setUserInformation({
+      infos: dataUser,
+      repos: dataRepo,
+    });
+  };
+
+  return (
+    <button
+      type="button"
+      onClick={() => saveUsersInformation(searchInputValue)}
+    >
+      Buscar
+    </button>
+  );
+};
 
 export default SearchButton;
