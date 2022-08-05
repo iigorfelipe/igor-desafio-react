983fb8cbe68d4a1b4aa61df9df0126011a380a90
Igor <iigorfelipe@gmail.com>
2022-08-05 11:44:19 -0300

    feat: creates in Main a base HTML structure to be able to work

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,12 +1,57 @@
-import React from "react";
+import React, { useContext } from "react";
 import SearchButton from "../components/SearchButton";
 import SearchInput from "../components/SearchInput";
+import GithubContext from "../providers/GithubContext";
 
 const Main = () => {
+  const {
+    userInformation: { infos, repos },
+  } = useContext(GithubContext);
+
+  console.log("INFOS>>>>", infos);
+  console.log("");
+  console.log("REPOS>>>>", repos);
+
   return (
     <div>
-      <SearchInput />
-      <SearchButton />
+      <div>
+        <SearchInput />
+        <SearchButton />
+      </div>
+
+      <div>
+        <img src={infos.avatar_url} alt="" />
+
+        <div>
+          <span>{infos.name}</span>
+          <span>{"user"}</span>
+        </div>
+
+        <button>follow</button>
+
+        <div>
+          <span>{"@user"}</span>
+
+          <div>
+            <span>icon {infos.followers} followers</span>
+            <span>{infos.following} following</span>
+            <span>icon {}</span>
+          </div>
+
+          <span>icon {infos.email}</span>
+        </div>
+      </div>
+
+      <div>
+        <span>icon Repositories {repos.length}</span>
+        {repos.map((item, index) => (
+          <div key={index}>
+            <span>{item.name}</span>
+            <span>{item.language}</span>
+            <span>{item.created_at}</span>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
