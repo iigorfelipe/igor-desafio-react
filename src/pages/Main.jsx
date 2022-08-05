09183a09a2cb305a8a1b3feb8f62b65f600e64e2
Igor <iigorfelipe@gmail.com>
2022-08-04 21:55:34 -0300

    feat: create folder to pages and add Main page

---
+++ src/pages/Main.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import SearchButton from "../components/SearchButton";
+import SearchInput from "../components/SearchInput";
+
+const Main = () => {
+  return (
+    <div>
+      <SearchInput />
+      <SearchButton />
+    </div>
+  );
+};
+
+export default Main;
