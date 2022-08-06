26f343037cb307273c04bddb58b10bba1029b0ac
Igor <iigorfelipe@gmail.com>
2022-08-06 16:19:20 -0300

    fix: remove BrouserRouter from index.jsx and put it in AppRoutes

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,13 +1,15 @@
 import React from "react";
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, BrowserRouter } from "react-router-dom";
 import SearchUser from "../pages/SearchUser";
 import Main from "../pages/Main";
 
 const AppRoutes = () => (
-  <Routes>
-    <Route path="/" element={<SearchUser />} />
-    <Route path="/repository" element={<Main />} />
-  </Routes>
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<SearchUser />} />
+      <Route path="/repository" element={<Main />} />
+    </Routes>
+  </BrowserRouter>
 );
 
 export default AppRoutes;
